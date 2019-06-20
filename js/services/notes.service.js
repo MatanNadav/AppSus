8b3c5f35da31b94628c5c741bcab596ec3069b88
Matan Nadav <Smatannadav@gmail.com>
2019-06-20 11:41:25 +0300

    notes-app

--- js/services/notes.service.js
+++ js/services/notes.service.js
@@ -1,27 +1,27 @@
 'use strict';
 import notesData from './data/mock-notes-data.js';
-import storageService from './storage.service.js'
+import { storageService } from '../services/storage.service.js'
 export const notesService = {
     query,
 }
 const NOTES_KEY = 'missNotes'
-console.log(notesData);
-function query(filter){
-     let notes =  storageService.load(NOTES_KEY);
-     if(notes){
-         notesData = notes;
-     }
-     storageService.store(NOTES_KEY,notesData);
-     return Promise.resolve(notesData);
+// console.log(notesData);
+function query(filter) {
+    let notes = storageService.load(NOTES_KEY);
+    if (notes) {
+        notesData = notes;
+    }
+    storageService.store(NOTES_KEY, notesData);
+    return Promise.resolve(notesData);
 
 }
-function create(){
-return  {
-     id: 1,
-     text: 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.', 
-     date: '1/3/2019',
-      time: '',
-      img:''
-     }
+function create() {
+    return {
+        id: 1,
+        text: 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.',
+        date: '1/3/2019',
+        time: '',
+        img: ''
+    }
 
 }
