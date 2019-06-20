6da8b38e9791d546cbb28390934e89b7624c82b4
Matan Nadav <Smatannadav@gmail.com>
2019-06-20 11:10:41 +0300

    notes app

---
+++ pages/notes/notes-app.cmp.js
@@ -0,0 +1,26 @@
+'use strict';
+
+export default {
+    template: `
+
+    `,
+
+    data() {
+        return {
+
+        }
+    },
+
+    props:[],
+    computed: {
+
+    },
+
+    methods: {
+
+    },
+
+    created() {
+
+    }
+}
